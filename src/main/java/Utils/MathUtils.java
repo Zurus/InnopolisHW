7b97ca2eb05580b46c0b8f0d7581c2804c0ee7c6
Divaev Artur <a.divaev.stc@innopolis.university>
2020-10-16 18:55:11 +0300

    Second commit

---
+++ src/main/java/Utils/MathUtils.java
@@ -0,0 +1,17 @@
+package Utils;
+
+import java.util.Random;
+
+public class MathUtils {
+    private final static Random RANDOM = new Random();
+
+
+    public static Number [] initArrayNumber(int count) {
+        Number[] result = new Number[count];
+        for (int i = 0; i < count ; i++) {
+            result[i] = 100 * RANDOM.nextDouble();
+        }
+        return result;
+    }
+
+}
